d4ebf67876b00ebcf50bfe10f4d3cd3664cb2462
Pavlo Syzonenko <pavlo.syzonenko@gmail.com>
2023-06-18 16:44:15 +0300

    initial commit

---
+++ middleware/error-handler.js
@@ -0,0 +1,7 @@
+const CustomAPIError = require('../errors/custom-errors');
+const errorHandlerMiddleware = (err, req, res, next) => {
+  if (err instanceof CustomAPIError) {
+    return res.status(err.statusCode).json({ message: err.message });
+  }
+  return res.status(500).send('Something went wrong, please try again');
+};
